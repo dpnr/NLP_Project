0f15ae94124c890815d89edf11a0efd63be762dc
khatwani <khatwani@lab1-10.eng.utah.edu>
2017-11-26 18:37:55 -0700

    added indiv

--- nlpProject.py
+++ nlpProject.py
@@ -73,7 +73,7 @@ for data in testData:
     print ("WEAPON:\t"+ "\n\t".join(fileArguments["weapon"]))
     
     
-    fileArguments["perp indiv"] = "-"#e_perpindiv.extracting('temfile.txt') ## replace this with some function call to get the right result
+    fileArguments["perp indiv"] = e_perpindiv.extracting('temfile.txt') ## replace this with some function call to get the right result
     print ("PERP INDIV:\t"+ fileArguments["perp indiv"])
 
     fileArguments["perp org"] = e_perporg.extracting('temfile.txt') ## replace this with some function call to get the right result
